f4169f5bcd8c24d86b29a7cc9da3e961b71e4a5a
testproject1 <test@project1.com>
2026-05-26 20:26:45 +0530

    first commit - counter app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const handleClickInc = () => {
+    let x = count;
+    setCount(++x)
+  }
+
+  const handleClickDec = () => {
+    let x = count;
+    setCount(--x);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{count}</h1>
+        <div className='sideBySide'>
+        <button onClick={handleClickInc}className='buttonInc'>Increment</button>
+        <button onClick={handleClickDec} className='buttonDec'>Decrement</button>
+        </div>
       </header>
     </div>
   );
